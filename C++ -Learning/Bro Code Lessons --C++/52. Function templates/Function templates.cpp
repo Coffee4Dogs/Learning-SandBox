303bd7277947c28b3aa32ebab037fbbe94723541
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-01-27 16:02:39 -0600

    .
    .

--- C++ -Learning/Bro Code Lessons --C++/52. Function templates/Function templates.cpp
+++ C++ -Learning/Bro Code Lessons --C++/52. Function templates/Function templates.cpp
@@ -1,6 +1,36 @@
+// function template = describes what a function looks like.
+//                     Can be used to generate as many overloaded functions
+//                     as needed, each using different data types.
+
+
 #include <iostream>
 
+// int max(int x, int y){
+//     return (x > y) ? x : y;
+// }
+
+// double max(double x, double y){
+//     return (x > y) ? x : y;
+// }
+
+// char max(char x, char y){
+//     return (x > y) ? x : y;
+// }
+
+// A template is a cookie-cutter that specifies how to cut cookies 
+// that all look pretty much the same
+// (although the cookies can be made of various kinds of dough, 
+// they’ll all have the same basic shape).
+
+template <typename T, typename U>
+
+auto max(T x, U y){
+    return (x > y) ? x : y;
+}
+
 int main() {
-    
+    //no mixing, ok like 1, 1.2
+    std::cout << max(1, 2.1) << '\n';
+
     return 0;
 }
---
+++ C++ -Learning/Programacion 3/test_delete_me.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x;
+    int total = 1;
+    x++;
+    total += x;
+    std::cout << total << '\n';
+
+    return 0;
+}
