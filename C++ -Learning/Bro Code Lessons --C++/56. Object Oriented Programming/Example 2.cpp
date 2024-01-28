a25aaa54932a39f0ed35a63cf73929c0d2ca57b1
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-01-28 14:01:11 -0600

    f

---
+++ C++ -Learning/Bro Code Lessons --C++/56. Object Oriented Programming/Example 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Car{
+    public:
+        std::string make;
+        std::string model;
+        int year;
+        std::string color;
+
+    void accelerate(){
+        std::cout << "You are on the gas!" << '\n';
+    }
+
+    void brake(){
+        std::cout << "You step on the brakes!" << '\n';
+    }
+
+};
+
+int main() {
+
+    Car car1;
+    
+
+    car1.make = "Ford";
+    car1.model = "Mustang";
+    car1.year = 2023;
+    car1.color = "silver";
+
+    std::cout << car1.make << '\n';
+    std::cout << car1.model << '\n';
+    std::cout << car1.year << '\n';
+    std::cout << car1.color << '\n';
+
+    return 0;
+}
--- C++ -Learning/Programacion 3/test_delete_me.cpp
+++ C++ -Learning/Programacion 3/test_delete_me.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 
-int main() {
-    int x;
-    int total = 1;
-    x++;
-    total += x;
-    std::cout << total << '\n';
 
+
+class Phone{
+    
+    std::string Color; //negro, azul, blanco
+    int Camera; //20, 19
+    std::string Model; //iPhone, Android
+    int Year; //2020, 2021, 2022, 2023, 2024
+
+    //Constructor
+    Phone(){
+
+    }
+
+    Phone(std::string Color, int Camera, std::string Model, int Year){
+        Color = Color;
+        Camera = Camera;
+        Model = Model;
+        Year = Year;
+
+    }
+
+    //Destructor
+    ~Phone(){
+
+    }
+
+
+
+    
+
+
+
+};
+
+
+int main() {
+    
     return 0;
 }
