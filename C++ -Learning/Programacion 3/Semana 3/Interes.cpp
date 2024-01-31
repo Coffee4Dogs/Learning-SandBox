5fcea719fb54d1bd29afe9ba6f3af923f7e2825d
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-01-31 09:53:17 -0600

    gf

--- C++ -Learning/Programacion 3/Semana 3/Interes.cpp
+++ C++ -Learning/Programacion 3/Semana 3/Interes.cpp
@@ -12,20 +12,53 @@ using std::setw;
 using std::fixed;
 
 
+//Calculo de interes = //Tasa de interes
+//                     (360 * 12)
+//                         365
+
+
+double CalculoInteresAnual (double TasaInteresAnual){
+    return TasaInteresAnual / ((360*12) / 365);
+}
+
+
+double CalculoCuotaNivelada(double Capital, int Plazo, double TasaInteresAnual){
+    
+    double Exponente = pow((1 + TasaInteresAnual), (-1 * Plazo));
+    
+    return ((Capital * TasaInteresAnual) / 1 - Exponente);
+     
+
+
+}
+
+
+
+
+
 int main() {
-    double interesAnual, cuota, capital, interesMensual;
-    int plazo;
+    double TasaInteresAnual, Cuota, Capital, InteresMensual;
+    int Plazo;
 
     cout << fixed << setprecision(2) << setw(10);
     cout << "Ingreso de Datos " << endl;
+    
+    //...Parte 1
     cout << "Ingrese la tasa de interes anual: " ;  //Tasa Interes Anual
-    cin >> interesAnual;
+    cin >> TasaInteresAnual;
+    InteresMensual = CalculoInteresAnual(TasaInteresAnual);
+    std::cout << "Tasa Interes Mensual: " << InteresMensual << '\n';
+
+    //...Parte 2
     cout << "Ingrese la el monto de capital: " ; //A
-    cin >> capital;
+    cin >> Capital;
     cout << "Ingrese el plazo en meses: " ; //m
-    cin >> plazo;        
+    cin >> Plazo;       
+    Cuota = CalculoCuotaNivelada(Capital, Plazo, TasaInteresAnual);
+    std::cout << "Cuota: " << Cuota << '\n';
 
-    interesMensual = cuota = 0;
 
+    // interesMensual = cuota = 0;
+    
     return 0;
 }
