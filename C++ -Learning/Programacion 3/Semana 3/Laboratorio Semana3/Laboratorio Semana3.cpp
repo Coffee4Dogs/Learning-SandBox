6a91a65e2cd0ac6485d0a6d5c30a8ae1a64e2cf7
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-02-03 15:00:13 -0600

    .
    .

--- C++ -Learning/Programacion 3/Semana 3/Laboratorio Semana3/Laboratorio Semana3.cpp
+++ C++ -Learning/Programacion 3/Semana 3/Laboratorio Semana3/Laboratorio Semana3.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <string>
 
-
-
-using std::string;
-using std::cout;
-using std::endl;
-using std::cin;
-
-
+using namespace std;
+double SaldoCuenta;
 class CUENTA {
-    public:
+    private:
         int c_id;
         double c_saldo;
 	    string c_fecapert;
 
-    private:
-        CUENTA(int newId=0, double newSaldo=1000, string newFecha = "20240201") // yyyymmdd 
+    public:
+        CUENTA(int newId=0, double newSaldo=1000.0, string newFecha = "20240201") // yyyymmdd 
         {
             std::cout << "Inicializando Clase Base Cuenta \n";
             c_id = newId;
@@ -30,9 +24,23 @@ class CUENTA {
 
         void setSaldo(double nuevoSaldo) {
             c_saldo = nuevoSaldo;
+            c_saldo = SaldoCuenta;
+            
         }
 
-        void setString(string nuevaFecha){
+        void setSaldo (double valorR, double comision = 0.0){ // Yohasthin
+			int nuevoSaldo ;
+			if (comision <= 0) {
+				nuevoSaldo = valorR + c_saldo ; 
+			} else {
+				nuevoSaldo = c_saldo - (valorR + comision) ;
+			}
+            c_saldo = nuevoSaldo;
+            c_saldo = SaldoCuenta;
+			cout << "El nuevo saldo de la cuenta es : " << nuevoSaldo << endl ;
+		}
+
+        void setFecha(string nuevaFecha){
             c_fecapert = nuevaFecha;
         }
 
@@ -44,16 +52,27 @@ class CUENTA {
             return c_saldo;
         }
 
+        void getSaldo( string usuario){ // Victor
+            cout << "El nombre del usuario ingresado es: " << usuario<<endl;
+
+           // if (this->c_saldo > 1000){
+             if (c_saldo > 1000){
+                cout<<"El saldo de la cuenta es mayor al minimo"<<endl;
+            }else{
+                cout<<"El saldo de la cuenta es menor al minimo"<<endl;
+            }
+        }
+
         string getFecha(){
             return c_fecapert;
         }
 
         void getInfo() {
-            cout << "Informacion de la cuenta" << std::endl;
+            cout << "Informacion de la cuenta \n";
             cout << "Numero de Cuenta: " << c_id << endl;
             cout << "Saldo " << c_saldo << endl;
             cout << "Fecha Apertura " << c_fecapert << endl;
-            return getInfo();
+           // return getInfo();
         }
 };
 
@@ -64,15 +83,15 @@ class AHORRO : public CUENTA {
         AHORRO(double nuevaTasa=1.5)
         {
             std::cout << "Inicializando clase derivada Ahorro\n";
-
+            a_tasa = nuevaTasa;
         }
     
         double getTasa() { return a_tasa; }
 		
 		void getDetAhorro() {
-			cout << "Numero de Cuenta: " << c_id << endl;
-			cout << "Saldo " << c_saldo << endl;
-			cout << "Fecha Apertura " << c_fecapert << endl;
+			cout << "Numero de Cuenta: " << getId() << endl;
+			cout << "Saldo " << getSaldo() << endl;
+			cout << "Fecha Apertura " << getFecha() << endl;
 			cout << "Tasa  " << a_tasa << endl;
 			
 		}
@@ -80,21 +99,140 @@ class AHORRO : public CUENTA {
 
 };
 
+class MENORAHORRO: public AHORRO{
+    private:
+        int edad;
+        bool tutorEsCliente;
+        int cantTr;
+
+    public:
+        MENORAHORRO(){
+            // Andersson
+            //setSaldo(0.0);
+            edad = 1;
+            tutorEsCliente = 0;
+            cantTr = 0;
+        }
+
+        MENORAHORRO(int newCuenta, string newFecha, double newSaldo, int newEdad = 1, int newCantTr = 2){
+            setId(newCuenta);
+            //setSaldo(newSaldo);
+            setFecha(newFecha);
+            edad = newEdad;
+            tutorEsCliente = true;
+            cantTr = newCantTr;    
+        }
+
+        int getEdad() { return edad;}
+        void setEdad(int edadMenor) { edad = edadMenor;}
+
+        // gettutor setTutor  Miguel
+
+
+        // getCant  setCant  Josue
+        //getcant 
+        int getcant(){        
+            return cantTr;
+        }
+
+        // setCant 
+        void setCant(int newcantidad){
+            cantTr= newcantidad;
+        }
+
+        void getDetalleMenorAhorro() {
+            getDetAhorro();
+            cout << "Edad del titular: " << edad << endl;
+            cout << "El tutor es cliente: " << (tutorEsCliente ? "Si" : "No") << endl;
+            cout << "Cantidad de transacciones realizadas: " << cantTr << endl;
+        }
+
+};
+
+class PRESTAMO : CUENTA {
+    public:
+        double MontoPrestamo = 0.0;//c_saldo
+        double MontoCuota = 0.0;
+        int CuotasOriginales = 0;
+        int CuotasPagadas = 0;
+
+    public:
+        PRESTAMO(double MontoPrestamo, double MontoCuota, int CuotasOriginales, int CuotasPagadas){
+            this->MontoPrestamo = SaldoCuenta;
+            this->MontoCuota = MontoCuota;
+            this->CuotasOriginales = CuotasOriginales;
+            this->CuotasPagadas = CuotasPagadas;
+        }
+
+        // Metodos Set
+            void SetMontoPrestamo(double temp){
+                MontoPrestamo = temp;
+            }
+
+            void SetMontoCuota(double temp){
+                MontoCuota = temp;
+            }
+
+            void SetCuotasOriginales(int temp){
+                CuotasOriginales = temp;
+            }
+
+            void SetCuotasPagadas(int temp){
+            CuotasPagadas = temp;
+        }
+
+        //Metodos Get
+            double GetMontoPrestamo(){
+                return MontoPrestamo;
+            }
+
+            double GetMontoCuota(){
+                return MontoCuota;
+            }
+
+            int GetCuotasOriginales(){
+                return CuotasOriginales;
+            }
+
+            int GetCuotasPagadas(){
+            return CuotasPagadas;
+        }
+
+        //Otros Metodos
+        void PreguntarCuotasOriginales(){
+            int Cuotas;
+            std::cout << "Escriba la cantidad de cuotas originales: " << '\n';
+            std::cin >> Cuotas;
+            SetCuotasOriginales(Cuotas);
+        }
+
+
+
+    PRESTAMO(){}
+    ~PRESTAMO(){}
+};  
+
+
+
 int main() {
-
+    PRESTAMO Prestamo1;
     std::cout << "Prueba de Programa y Clases" << std::endl;
-
-    
 	CUENTA General(100, 2500, "20231026");
-	
-
 	General.getInfo();
-	
-	// AHORRO AhorroBase(200, 1500, "20231027",1.25);
-	AHORRO AhorroBase(1.25);
-
+	//AHORRO AhorroBase(200, 1500, "20231027",1.25);
+    AHORRO AhorroBase(1.25);
 	AhorroBase.getDetAhorro();
+    MENORAHORRO HijoAhorro;
+    HijoAhorro.getDetalleMenorAhorro();
+    HijoAhorro.setSaldo(250, 10);
+    HijoAhorro.setSaldo(100, 0);
 	
-	
-    return;
+
+    // std::cout << "El saldo es: "<< General.getSaldo() << '\n';
+    Prestamo1.PreguntarCuotasOriginales();
+    Prestamo1.SetMontoPrestamo(General.getSaldo());
+    std::cout << "El saldo es: "<< Prestamo1.GetMontoPrestamo() << '\n';
+
+
+    return 0;
 }
--- C++ -Learning/Programacion 3/test_delete_me.cpp
+++ C++ -Learning/Programacion 3/test_delete_me.cpp
@@ -1,42 +1,112 @@
 #include <iostream>
 
+class Instrument{
+    private:
+        std::string Color = "No Color Defined";      // Red, Blue, Black, Orange, etc..
+        double Width = 0.0;                         // 3.5, 4.8, 8.2
+        double Height = 0.0;                        // 3.5, 4.8, 8.2
+        std::string Tone = "No Tone";               //C5 D5 E5 F5 G5 A5 B5
+        bool IsInstrument = true;
+
+    public:
+        Instrument(std::string Color, double Width, double Height, std::string Tone, bool IsInstrument){
+        this->Color = Color;    
+        this->Width = Width;          
+        this->Height = Height;     
+        this->Tone = Tone;
+        this->IsInstrument = IsInstrument;
+        }
+
+        //Set Functions
+            void SetColor(std::string temp){
+                Color = temp;
+            }
+           
+            void SetWidth(double temp){
+                Width = temp;
+            }
+            
+            void SetHeight(double temp){
+                Height = temp;
+            }
+            
+            void SetTone (std::string temp){
+                Tone = temp;
+            }
+            
+            bool SetIsInstrument(bool temp){
+                IsInstrument = temp;
+            }
 
 
-class Phone{
+        //Get Functions
+            std::string GetColor(){
+                return Color;
+            }
+
+            double GetWidth(){
+                return Width;
+            }
+
+            double GetHeight(){
+                return Height;
+            }
+
+            std::string GetTone(){
+                return Tone;
+            }
+
+            bool GetIsInstrument(){
+                return IsInstrument;
+            }
+
+
     
-    std::string Color; //negro, azul, blanco
-    int Camera; //20, 19
-    std::string Model; //iPhone, Android
-    int Year; //2020, 2021, 2022, 2023, 2024
+        void PlayMusic(){
+            std::cout << "You are playing music! :D" << std::endl;
+        }
 
-    //Constructor
-    Phone(){
+        void AboutInstrument(){
+            std::cout << "You are playing an instrument color: "<< Color << '\n';
+        }
 
-    }
+    Instrument(){}
+    ~Instrument(){}
+};
 
-    Phone(std::string Color, int Camera, std::string Model, int Year){
-        Color = Color;
-        Camera = Camera;
-        Model = Model;
-        Year = Year;
+class ChordString : public Instrument{
+    private: 
+        int NumberStrings = 0;
 
-    }
+    public:
+        ChordString(int NumberStrings){
+            this->NumberStrings = NumberStrings;
+        }
 
-    //Destructor
-    ~Phone(){
-
-    }
-
-
-
-    
-
+        void SetNumberStrings(int temp){
+            NumberStrings = temp;
+        }
+        
+        int GetNumberStrings(){
+            return NumberStrings;
+        }
 
 
+
+    ChordString(){}
+    ~ChordString(){}
 };
 
 
+
 int main() {
+    Instrument Headphones;
+    ChordString Guitar;
+    Headphones.SetColor("Red");
+    std::cout << "Your headphones are: " << Headphones.GetColor() << '\n';
+    
+    Guitar.SetColor("Dark");
+    std::cout << "Your Guitar is: " << Guitar.GetColor() << '\n';
     
     return 0;
 }
