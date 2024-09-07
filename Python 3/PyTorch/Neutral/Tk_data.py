f4904ca67c4b10fc2211b6d9e8730c7224725367
Coffee4Dogs <59121551+Coffee4Dogs@users.noreply.github.com>
2024-09-07 12:29:41 -0600

    .
    .

---
+++ Python 3/PyTorch/Neutral/Tk_data.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+window = Tk()
+
+
+# List of colors used:
+colors = ["#1a1a1a", "#0f0f0f", "#1c1c1c", "#d4d4d4", "#ffffff", "#ed152f"]
+
+# Settings of the program:
+window.title("This is my title, Lorem Ipsum")
+window.geometry("800x500")
+window.config(background=colors[0])
+
+
+
+
+
+
+
+
+window.mainloop()
